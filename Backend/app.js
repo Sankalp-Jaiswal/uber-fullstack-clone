e6379ad18167dfcfd3f11e5cce9f59d1bc1aca24
Sankalp-Jaiswal <sankalpjaiswal264@gmail.com>
2024-12-23 23:55:35 +0530

    day1

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+export default app;
